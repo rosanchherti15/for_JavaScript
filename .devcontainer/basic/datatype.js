15dc27dcfac158b8f07021bfd4b0492cb95d793a
Rosan Chhetri <kcroshan352@gmail.com>
2025-05-05 13:22:36 +0000

    datatypes

---
+++ .devcontainer/basic/datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert ( 3 + 3) // we are using node.js, not browser
+
+console.log( 3 
+    +
+     3) // code readbility should be high 
+
+console.log("Rosan")
+
+
+let name = "Rosan"
+let age = 19
+let IsloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standard value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof "Rosan");
+
+
+console.log(typeof age);
+
+
