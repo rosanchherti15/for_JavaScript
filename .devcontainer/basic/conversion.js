62d7bb78817c8f1fe5f32bd6b2720caee91d0da6
Rosan Chhetri <kcroshan352@gmail.com>
2025-05-19 11:34:21 +0000

    ConversionofDatatype

---
+++ .devcontainer/basic/conversion.js
@@ -0,0 +1,38 @@
+// let score = 33;
+// let score = "33abc";
+// let score = null
+// let score = undefined;
+// let score = false/true;[ 0/1 ]
+let score = "rosan";
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1 /false => 0
+*/
+
+//let isLoggedIn = 1
+let isLoggedIn = ""
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// "" => flase
+// "rosan" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
