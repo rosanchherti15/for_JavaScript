0c925bfebbf3562090202896a0f3e422cd61cbb4
Rosan Chhetri <kcroshan352@gmail.com>
2025-05-20 13:07:29 +0000

    operation.js

---
+++ .devcontainer/basic/operation.js
@@ -0,0 +1,41 @@
+let value = 3
+let NegValue = -value
+// console.log(NegValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "rosan"
+let str2 = "chhetri"
+let str3 = str1+str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2+2
+
+let gmaeCounter = 100;
+gmaeCounter++;
+console.log(gmaeCounter);
+
+
+
+
+
+
